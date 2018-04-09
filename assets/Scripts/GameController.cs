05f7b3af6d20a0208bc18b1d3db5b2a9124ab331
PeterAdamson <adamsonpeterj@gmail.com>
2018-04-09 13:22:02 -0300

    Add files via upload

---
+++ assets/Scripts/GameController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Text endText;
+	public Text scoreText;
+	private int remaining;
+
+	// Use this for initialization
+	void Start () {
+		endText.text = "";
+		remaining = 3;
+		scoreText.text = "Obtacles Remaining: " + remaining.ToString();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.R))
+		{
+			Time.timeScale = 1;
+			SceneManager.LoadScene( SceneManager.GetActiveScene().name );
+		}
+
+		if(Input.GetKeyDown(KeyCode.Escape))
+		{
+			Application.Quit();
+		}
+	}
+
+	public void GameOverLose()
+	{
+		endText.text = "You Lose";
+		Time.timeScale = 0;
+	}
+
+	public void GameOverWin()
+	{
+		endText.text = "You Win!";
+		Time.timeScale = 0;
+	}
+
+	public void addScore()
+	{
+		remaining -= 1;
+		scoreText.text = "Obtacles Remaining: " + remaining.ToString();
+		if(remaining <= 0)
+		{
+			GameOverWin();
+		}
+	}
+}
